ebb49e5ae5e46d35c9d6fccba47234a2484b9335
skyemperor611 <blackmonarch124@gmail.com>
2023-11-26 20:19:07 +0900

    crud 완료 시큐리티 작성 시작

---
+++ src/main/java/com/blog/my/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.blog.my.service;
+
+import com.blog.my.domain.User;
+import com.blog.my.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(()-> new IllegalArgumentException((email)));
+    }
+}
