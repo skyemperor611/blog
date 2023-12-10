5c2570abaee3b7cb775755017246834e28ac993c
skyemperor611 <blackmonarch124@gmail.com>
2023-12-11 01:00:11 +0900

    oauth2 작성중

--- src/main/java/com/blog/my/service/UserService.java
+++ src/main/java/com/blog/my/service/UserService.java
@@ -13,16 +13,23 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+
     public Long save(AddUserRequests dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
-                .password(passwordEncoder.encode(dto.getPassword())
-                ).build()).getId();
+                .password(encoder.encode(dto.getPassword()))
+                .build()).getId();
     }
 
     public User findById(Long userId) {
         return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
